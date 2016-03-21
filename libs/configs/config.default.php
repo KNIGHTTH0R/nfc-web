2db854a63d821a7376f9be45e009d9f9cba0a648
Martin Gold <martin.gold@volny.cz>
2016-03-21 17:48:25 +0100

    Configuration changes

--- libs/configs/config.default.php
+++ libs/configs/config.default.php
@@ -1,5 +1,12 @@
 <?php
 
+  /* Instructions
+    -Copy this to the same folder under name 'config.php'
+    -Change values to match your database
+    -Run the synchronize.php in the root folder
+    -Delete synchronize.php and nfc.sql
+  */
+
   $db_config = array(
             "host"        => "host",
             "user"        => "user",
